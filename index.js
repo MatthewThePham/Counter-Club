4597b31798b4e3580cb93b0346d3cc378d785c02
Matthew Pham <matto475@gmail.com>
2018-06-22 08:35:19 +0000

    Improvements to string behavior. Added different score messages based on what the user got. Pauses are improved. Personality added.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const LaunchRequestHandler = {
     const sessionAttributes = {};
 
     sessionAttributes.score= 0;
-    sessionAttributes.arrayAmount= 6;
+    sessionAttributes.arrayAmount= 5;
     sessionAttributes.gameState= 'ongoing';
     sessionAttributes.correctRandomNum= 0;
     sessionAttributes.stateDetermine= false;
@@ -24,10 +24,11 @@ const LaunchRequestHandler = {
      //sets the default list to 5 numbers and score to 0
      attributesManager.setSessionAttributes(sessionAttributes);
 
-     const startPrompt= 'Hello fellow human, and welcome to Counter! This is a memory test, where a list of integer numbers will be given to you.'
-     + ' Your job is to say yes or no, depending if the number was in that list. Ready? Begin! ' + stringArray(handlerInput);
+     const startPrompt= 'Greetings, staff volunteer, and welcome to the Counter campus!'
+     + 'Today, we will facilitate a memory test, where a list of integer numbers will be read off to you.'
+     + ' Your job is to say, yes, or , no, depending if a given number was in that list , . Ready , ? Begin! ' + stringArray(handlerInput);
 
-    const reprompt = 'If you need help deciding, you could always flip a quarter.';
+    const reprompt = ' , If you need help deciding, some volunteers flip a coin in desperation. ';
 
      return handlerInput.responseBuilder
      .speak(startPrompt)
@@ -42,9 +43,9 @@ const LaunchRequestHandler = {
     return request.type === 'IntentRequest' && request.intent.name === 'AMAZON.HelpIntent';
   },
   handle(handlerInput) {
-    const speechOutput = 'Please repond with ,yes or no, depending if the given number was in that list.'
-    +'If you need help deciding, you could always flip a nickle. Heads for yes, tails for no.';
-    const reprompt = 'I would recommend the coinflip.';
+    const speechOutput = 'Again, respond with ,yes or no, depending if the given number was in that list , . '
+    +' If you need help deciding, you could always flip a coin, as we do not repeat the list';
+    const reprompt = ' Heads for yes, tails for no.';
 
     return handlerInput.responseBuilder
     .speak(speechOutput)
@@ -61,7 +62,7 @@ const ExitHandler = {
  },
  handle(handlerInput) {
   return handlerInput.responseBuilder
-  .speak('Taking an early leave? See you next time.')
+  .speak(' Leaving so soon? Thank you for participating, the counter experiment. ')
   .getResponse();
 },
 };
@@ -77,13 +78,13 @@ const YesIntentHandler = {
 
    if(sessionAttributes.gameState != 'ongoing'){
         //means the user said yes and the game is over aka they want to restart the game
-        var restartPrompt = 'New round, new me. Begin! ';
+        var restartPrompt = ' That is the spirit! Let us begin. ';
         var reprompt = 'Say, yes or no, depending if the given number was in that list.'
-        +',If you need help, try flipping a coin?';
+        +' ,If you need help, some volunteers like to visualize the numbers.';
 
         sessionAttributes.gameState= 'ongoing';
         sessionAttributes.score = 0;
-        sessionAttributes.arrayAmount = 6;
+        sessionAttributes.arrayAmount = 5;
 
         return handlerInput.responseBuilder
         .speak(restartPrompt+ stringArray(handlerInput))
@@ -108,7 +109,7 @@ const YesIntentHandler = {
 
      if(sessionAttributes.gameState != 'ongoing'){
         //means the user said no and the game is over aka they want to end the game
-        var exitPrompt = ' The test has concluded. I hope I will see you next time!';
+        var exitPrompt = ' The test has concluded. Come back anytime, for another counter experiment!';
 
         return handlerInput.responseBuilder
         .speak(exitPrompt)
@@ -117,7 +118,6 @@ const YesIntentHandler = {
       else{
         return checkAnswer(handlerInput, answer);
       }
-
 
     },
   };
@@ -137,7 +137,7 @@ const YesIntentHandler = {
       return true;
     },
     handle(handlerInput) {
-      const respondSpeech = 'Sorry but all I can understand is yes and no. Please say yes or no if the number was on the list';
+      const respondSpeech = ' Sorry but all I can understand is yes and no. Please say yes or no if the number was on the list';
       return handlerInput.responseBuilder
       .speak(respondSpeech)
       .reprompt(respondSpeech)
@@ -153,13 +153,11 @@ const YesIntentHandler = {
       console.log('Error handled: ${error.message}');
 
       return handlerInput.responseBuilder
-      .speak('Sorry, I can\'t understand the command. Speak loud and clear.')
-      .reprompt('Sorry, I can\'t understand the command. Speak a bit louder and clearer.')
+      .speak(' Sorry, I can\'t understand the command. Speak loud and clear.')
+      .reprompt(' Sorry, I can\'t understand the command. Speak a bit louder and clearer.')
       .getResponse();
     },
   };
-
-
 
 //makes a string output and creates a correct number value
 function stringArray (handlerInput){
@@ -167,30 +165,26 @@ function stringArray (handlerInput){
 
   var questions = []; 
   var outputString= '';
-  const NUMS_GIVEN = 20;
+  //range of possible output numbers in the array are the max and min
+  const max = 19;
+  const min=0;
 
-    //this is randomizer and target value between 0 and max number * 2 
-    const random = Math.floor(Math.random() * (NUMS_GIVEN) + 1);
-    sessionAttributes.correctRandomNum = random;
+  //this is randomizer and target value between 0 and max number 
+  const random = Math.floor(Math.random() * ((max-min)+1) + min);
+  sessionAttributes.correctRandomNum = random;
 
     for (let i = 0; i < sessionAttributes.arrayAmount-1; i++) {
-        //x and y is the range
-        const x=0; const y=20;
-
         //puts random values into a question String
-        questions[i] = Math.floor(Math.random() * ((y-x)+1) + x);
-        outputString = outputString + '<break time=".4s"/>' +  '<emphasis level="reduced"> '+ questions[i] +'</emphasis>';
+        questions[i] = Math.floor(Math.random() * ((max-min)+1) + min);
+        outputString = outputString + '<break time="1s"/>' +  '<emphasis level="reduced"> '+ questions[i] +'</emphasis>';
       } 
 
-
-    //when the user score reaches 3 add one random strings at the end of the array
-    if(sessionAttributes.score > 5){
-      var trollArray = ['kiwifruit','cherry','watermelon'];
+    //when the user score reaches every 4rd round add one random big integer at the end of the array
+    if(sessionAttributes.score % 4 == 0 && sessionAttributes.score != 0){
+      var trollArray = [321, 123 ,432];
       var troll = randomizedStrings(trollArray);
       questions.push(troll);
-      //If want to put in the middle, have to manipulate the top part of the code ie output string. questions.splice(2, 0, troll)
-      //will not work.
-      outputString = outputString + '<break time=".4s"/>' +  '<emphasis level="reduced"> '+ troll +'</emphasis>';
+      outputString = outputString + '<break time="1s"/>' +  '<emphasis level="reduced"> '+ troll +'</emphasis>';
     }
 
     //checks if the random number was in the list or not
@@ -200,7 +194,7 @@ function stringArray (handlerInput){
       sessionAttributes.stateDetermine = false;
     }
 
-    outputString = outputString + '<break time=".7s"/>' + ' Was the number ' + random + ' in the list?';
+    outputString = outputString + '<break time="1.2s"/>' + ' Was the number ' + random + ' in the list?';
     return outputString;
   }
 
@@ -213,14 +207,15 @@ function randomizedStrings(stringArray){
  //sees if user answer is correct or not and the game is ongoing
  function checkAnswer(handlerInput, answer) {
   const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-  var correctArray = ['bingo','booya','bravo'];
-  var redoArray = ['Would you like to regain your honor?','Want to redeem yourself?', 'Want to retry?'];
+  var correctArray = [' bingo',' booya',' bravo'];
+  var redoArray = [' Would you like to regain your honor?',' Want to redeem yourself?', ' Want to retake the test?'];
 
   if(answer == sessionAttributes.stateDetermine && sessionAttributes.gameState === 'ongoing'){
         //means the user answer is correct and the game is not over aka they will continue to the next round
-        var congratzPrompt = '<say-as interpret-as="interjection"> ' +  randomizedStrings(correctArray) + '</say-as>' + '! Nice one. Next round.'
-        var reprompt = 'Say yes or no depending if the given number was in that list.'
-        +'If you need help, try flipping a coin?';
+        var congratzPrompt = ' Commencing validation, .' + '<say-as interpret-as="interjection"> ' +  randomizedStrings(correctArray) + '</say-as>' 
+        + ' . Next round.  <break time=".4s"/>'
+        var reprompt = ' Say yes or no depending if the given number was in that list.'
+        +' If you need help, some volunteers like to focus on where the concept of numbers came from.';
 
         sessionAttributes.score = sessionAttributes.score + 1;
         sessionAttributes.arrayAmount = sessionAttributes.arrayAmount + 1 ;
@@ -232,13 +227,39 @@ function randomizedStrings(stringArray){
       }
       else{
         //means the user said no and the game is ongoing 
-        var correctNum= sessionAttributes.correctRandomNum;
-
-        var sadPrompt = 'Sorry. The correct number was actually' + correctNum + '. The results are in, you scored a ' 
-        + sessionAttributes.score +' . ' + randomizedStrings(redoArray);
-        var reprompt = 'Say yes or no if you want to play again.';
-
+        var correctNum = sessionAttributes.correctRandomNum;
+        var defaultRank = 'NaN';
+        var sadPrompt = 'NaN';
+        var reprompt = ' Say yes or no if you want to play again.';
         sessionAttributes.gameState = 'ended';
+
+        //congradulatory message based on what score the user got
+        if(sessionAttributes.score <= 4){
+          defaultRank = ' . , Unfortunately, your memory is, below the minimum baseline. But, I am sure there is room for improvement. ';
+        }
+        else if(sessionAttributes.score >= 4){
+          defaultRank = ' . Congratulation, your memory is, just at the required baseline. Good job for being common . ';
+        }
+        else if(sessionAttributes.score >= 8){
+          defaultRank = ' . , Impressive, either you are very, good at guessing, or you have excellent memory. You have surpassed the regular volunteer. ';
+        }
+        else if(sessionAttributes.score >= 13){
+          defaultRank = ' . , Wow, you have a strong memory capacity. Most volunteers fail to reach this rank. ';
+        }
+        else if(sessionAttributes.score >= 18){
+           defaultRank = ' . , This is the end of the ranking system. You have the honor of being the brightest volunteer of the month. ';
+        }
+
+        //changes the correct num message
+        if(answer == false ){
+          //means they said no and are wrong
+          sadPrompt = 'Sorry. But , ' + correctNum + ', was in the list. Your score results are in, you scored a , ';
+        }
+        else{
+          //means they said yes and are wrong
+          sadPrompt = 'Sorry. But , ' + correctNum + ', was not in the list. Your score results are in, you scored a , ';
+        }
+        sadPrompt = sadPrompt + sessionAttributes.score + defaultRank + randomizedStrings(redoArray);
 
         return handlerInput.responseBuilder
         .speak(sadPrompt)
@@ -246,7 +267,6 @@ function randomizedStrings(stringArray){
         .getResponse();
       }
     }
-
 
     const skillBuilder = Alexa.SkillBuilders.custom();
 
