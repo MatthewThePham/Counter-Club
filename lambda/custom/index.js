6defa154cf6d340bcc5b1a96982146125a103e2f
Matthew Pham <matto475@gmail.com>
2018-09-11 04:54:33 +0000

    +Commented on each important section

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,37 +1,52 @@
-  //counter alexa application
+  //load ask-sdk libraries
   const Alexa = require('ask-sdk');
+  //string variables for if the user says something outside the intent model, IE they say something totally random.
   const FALLBACK_MESSAGE = ' Sorry , but yes , or no, is all I can understand. Please say ,yes ,or , no , . ';
   const FALLBACK_REPROMPT = ' Please say ,yes ,or , no , . ';
 
-  //starts the app from this handler
   const LaunchRequestHandler = {
       canHandle(handlerInput) {
           const request = handlerInput.requestEnvelope.request;
+          //The 'LaunchRequest' request type will start the the skill from this handler
           return request.type === 'LaunchRequest';
       },
       async handle(handlerInput) {
+        //Creates attributeManager to set session attributes. Session attributes are variables that exist throughout the skill's sesssion, aka when the user is still using the skill.
+        //The session attribute variable will be gone after the user exits the skill, unless it is saved onto a database, making the data permanent.
+        //In our skill, we will use DyamboDB for saving permanent user data. (Our skill saves the user's high score aka an Integer value to DyamboDB) 
+        
           const attributesManager = handlerInput.attributesManager;
-          var sessionAttributes = await attributesManager.getPersistentAttributes() || {};
+        //Using await attributesManager.getPersistentAttributes() is called to fetch the user's high score. If there is no high score, set the sessionAttributes to an empty array.
+        //HINT, if you want to use await, you have to have async with it as well.
+        //We use async/await to make sure the database data is properly fetched. Async will return a promise, and await pause the skill, and wait until the promise returns a value. 
+          var sessionAttributes = await attributesManager.getPersistentAttributes() || {}; 
+        
+        //This is the default start prompt if the user has not played yet.
           var startPrompt = ' Welcome, have you come to gain wisdom? ' +
               ' Here at the Counter society, we improve our minds daily, by giving out memory exercises , where a list of numbers will be given to you.' +
               ' All you have to do is say, yes, or , no, depending if a given number was inside that list . I hope you are ready ,  as your exercise is about to begin! ';
           const reprompt = ' , If you need help deciding, just relax and let the numbers flow through you. ';
 
-          if (Object.keys(sessionAttributes).length === 0) { //sets the intial user score to zero 
+          //Sets the intial user score to zero if it is the first time the user has played, aka the sessionAttributes is set to an empty array.
+          if (Object.keys(sessionAttributes).length === 0) { 
               sessionAttributes.highScore = 0;
           }
-
-          sessionAttributes.score = 0; //other sessionAttributes reset every time
+          
+         //other sessionAttributes reset every time, that is not high score.
+         //sets the default list to 5 numbers and current score to 0
+         //POSSIBLE improvement? Instead of saving all sessionAttributes, you could just pinpoint one, such as sessionAttributes.highScore          
+          sessionAttributes.score = 0; 
           sessionAttributes.arrayAmount = 6;
           sessionAttributes.gameState = 'ongoing';
           sessionAttributes.correctRandomNum = 0;
           sessionAttributes.stateDetermine = false;
-
-          //sets the default list to 5 numbers and score to 0
+          
+          //sets all the sessionAttributes variables to attributesManager, which is a built in function from ask-sdk. 
           attributesManager.setSessionAttributes(sessionAttributes);
 
+        //compares the high score values, depending on what the user got, they get a welcome message.
           if (sessionAttributes.highScore != 0) {
-              var defaultRank = ' human infant. '; //user got a score less than 3
+              var defaultRank = ' human infant. ';  //user got a score less than 3
               if (sessionAttributes.highScore >= 18) {
                   defaultRank = ' expert math genius. '
               } else if (sessionAttributes.highScore >= 12) {
@@ -41,11 +56,16 @@
               } else if (sessionAttributes.highScore >= 5) {
                   defaultRank = ' common human. ';
               }
-
+            
+            //We will override the default start prompt if the user has earned a score.
               startPrompt = ' Welcome back, to the counter society. Your current benchmark is , ' + defaultRank +
                   ' , or a numbered ranking of ' + sessionAttributes.highScore.toString() + ' out of 18 genius points. ' + ' . Get ready , for another mind exercise! ';
           }
 
+        //This will control what the Alexa skill says.
+        //speak is what is said intially, reprompt is a repeat if the user has not responded in the last few seconds.
+        //HINT, if reprompt() is not called, the session will end right after the speak. IE your skill will say what is the speak output, and exit not taking user input.
+        //getResponse() will end the reponse builder call
           return handlerInput.responseBuilder
               .speak(startPrompt + stringArray(handlerInput))
               .reprompt(reprompt)
@@ -56,6 +76,9 @@
   const HelpIntentHandler = {
       canHandle(handlerInput) {
           const request = handlerInput.requestEnvelope.request;
+          //The 'IntentRequest' request type will match what json inputs are listed for the intent name. 
+          //In this case, we used a prebuilt in intent, called AMAZON.HelpIntent. You can tell, as it has the constant keyword AMAZON.
+        
           return request.type === 'IntentRequest' && request.intent.name === 'AMAZON.HelpIntent';
       },
       handle(handlerInput) {
@@ -81,6 +104,7 @@
 
           if (sessionAttributes.gameState != 'ongoing') {
               //means the user said yes and the game is over aka they want to restart the game
+            
               var restartPrompt = ' That is the spirit! Let us begin. ';
               var reprompt = 'Say, yes or no, depending if the given number was in that list.' +
                   ' ,If you need help, some people like to visualize the numbers.';
@@ -111,9 +135,11 @@
 
           if (sessionAttributes.gameState != 'ongoing') {
               //means the user said no and the game is over aka they want to end the game
+            
               var exitPrompt = ' Your session has concluded. Come back anytime, for another Counter exercise!' 
               + '<say-as interpret-as="interjection"> ' + ' ta ta. ' + '</say-as>';
-
+              
+              //will exit the skill, as reprompt is not called.
               return handlerInput.responseBuilder
                   .speak(exitPrompt)
                   .getResponse();
@@ -143,10 +169,13 @@
       },
       handle(handlerInput) {
           console.log('Session ended with ${handlerInput.requestEnvelope.request.reason}');
+        //Because there is no speak or reprompt, the handler will just end
           return handlerInput.responseBuilder.getResponse();
       },
   };
 
+//This handler will catch user input that cannot be understood, or ones that do not match the given intents you made. 
+//It will continue the skill, after saying the error message
   const ErrorHandler = {
       canHandle() {
           return true;
@@ -161,13 +190,13 @@
   };
 
   //as of 2018, only works in english united states. For some reason needs a custom intent for it to work. The only reason why unhandled intent was made in the json intents.
+  //Similiar to the error handler, but can be modified to have specific error messages based on what state it is in.
   const FallbackHandler = {
       canHandle(handlerInput) {
           const request = handlerInput.requestEnvelope.request;
           return request.type === 'IntentRequest' && request.intent.name == 'AMAZON.FallbackIntent';
       },
       handle(handlerInput) {
-          //const respondSpeech = ' Sorry but all I can understand is yes and no. Please say yes or no if the number was on the list';
           return handlerInput.responseBuilder
               .speak(FALLBACK_MESSAGE)
               .reprompt(FALLBACK_REPROMPT)
@@ -175,8 +204,10 @@
       },
   };
 
+  //HELPER FUNCTIONS
   //makes a string output and creates a correct number value
   function stringArray(handlerInput) {
+      //gets the getSessionAttributes data
       const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
       var questions = [];
       var outputString = '';
@@ -190,7 +221,11 @@
 
       for (let i = 0; i < sessionAttributes.arrayAmount - 1; i++) {
           //puts random values into a question String
+        
           questions[i] = Math.floor(Math.random() * ((max - min) + 1) + min);
+           //The '<break time=".6s"/>' is SSML, which allows for a more controlled pause, and '<emphasis level="reduced">' 
+           //allows emphasis on certain words.
+        
           outputString = outputString + '<break time=".6s"/>' + '<emphasis level="reduced"> ' + questions[i] + '</emphasis>';
       }
 
@@ -208,7 +243,7 @@
       } else {
           sessionAttributes.stateDetermine = false;
       }
-
+  
       outputString = outputString + '<break time="1.2s"/>' + ' Was the number ' + random + ' in the list?';
       return outputString;
   }
@@ -222,6 +257,7 @@
   //sees if user answer is correct or not and the game is ongoing
   async function checkAnswer(handlerInput, answer) {
       const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+    //arrays allow for more random sayings to allow your skill to a little different each time.
       var correctArray = [' bingo. ', ' booya. ', ' bravo. ', ' hurray. ', ' eureka. ', ' well done. ', ' Wowzer. '];
       var rightArray = [' flawless round. ', ' Killing spree. ', ' You nailed it. ', ' Plus one genius point. ', ' Another one bites the dust. ', ' Play of the game. ', ' What a legend. '];
       var failArray = [' le sigh. ', ' aw man. ', ' blast. ', ' oof. ', ' wah wah. '];
@@ -229,6 +265,9 @@
 
       if (answer == sessionAttributes.stateDetermine && sessionAttributes.gameState === 'ongoing') {
           //means the user answer is correct and the game is not over aka they will continue to the next round
+          //<say-as interpret-as="interjection">  is more SSML used to add more personality when saying the word.
+           //NOT ALL words are supported with interjection, could sound strange if the word is not supported.
+        
           var congratzPrompt = '<say-as interpret-as="interjection"> ' + randomizedStrings(correctArray) + '</say-as>' +
               randomizedStrings(rightArray) + ' . Next round.  <break time=".5s"/>'
           var reprompt = ' Say yes or no depending if the given number was in that list.' +
@@ -253,7 +292,11 @@
               ' Sorry. But , ' + correctNum;
           var reprompt = ' Say yes or no if you want to play again.';
 
-          if (sessionAttributes.score > sessionAttributes.highScore) {  //sets the user high score
+        //sets the user high score to a dyamboDB table
+        //handlerInput.attributesManager.setPersistentAttributes will set the newly updated high score from sessionAttributes.
+        //savePersistentAttributes will update the dyamboDB table.
+        
+          if (sessionAttributes.score > sessionAttributes.highScore) {  
               sessionAttributes.highScore = sessionAttributes.score;
 
               await handlerInput.attributesManager.setPersistentAttributes(sessionAttributes);
@@ -292,6 +335,7 @@
   //needed for DyamboDB
   const skillBuilder = Alexa.SkillBuilders.standard();
 
+//puts the handlers into the skillBuilder to use in your skill.
   exports.handler = skillBuilder
       .addRequestHandlers(
           LaunchRequestHandler,
@@ -303,6 +347,8 @@
           SessionEndedRequestHandler
       )
       .addErrorHandlers(ErrorHandler)
+    //'counter' is the DyamboDB table name that will be created. Make sure you allow for proper IAM permissions for lambda and dyamboDB.
+    //withAutoCreateTable will create a new table for us, if a DB table called 'counter' is not made yet.
       .withTableName('counter')
       .withAutoCreateTable(true)
       .lambda();
